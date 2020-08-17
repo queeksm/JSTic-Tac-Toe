df8c60a6d127cfe081ec65b5aece3098171c8a48
queeksm <queeksm@gmail.com>
2020-08-17 14:51:25 -0500

    Linting

--- scripts/index.js
+++ scripts/index.js
@@ -7,82 +7,81 @@ const board = gameBoard();
 
 let DOMControl = (function (){
   
-function writeInCell(i,j,value) {
-  document.getElementById(`${i}-${j}`).innerHTML = value;
-} 
-
-function remove_player_form(){
-  document.getElementById('buttonDiv').remove();
-  document.getElementById('saveButtonDiv').remove();
-  document.getElementById('myPlayerForm-1').innerHTML = '';
-  document.getElementById('myPlayerForm-2').innerHTML = '';
-  document.getElementById('StartGameButton').disabled = false;
-}
-
-function display_players() {
-  const playerDiv1 = document.getElementById('Player1');
-  const playerDiv2 = document.getElementById('Player2');
-
-  playerDiv1.innerHTML = `${player1.name}, You're ${player1.token}`;
-  playerDiv2.innerHTML = `${player2.name}, You're ${player2.token}`;
-}
-
-function getNameField(n) {
-  return document.getElementById('nameField-'+n).value;
-}
-
-function playerCapture() {
-  //const name1 = document.getElementById('nameField-1').value;
-  const name1 = getNameField(1);
-  const name2 = getNameField(2);
-  player1 = player(name1, 'X');
-  player2 = player(name2, 'O');
-
-  currentPlayer = player1;
-  remove_player_form();
-  display_players();
-}
-
-const formRender = (formDiv, PNumber) => {
-  formDiv.innerHTML = '';
-
-  const newPlayerForm = document.createElement('FORM');
-  newPlayerForm.setAttribute('id', `myPlayerForm-${PNumber}`);
-  formDiv.appendChild(newPlayerForm);
-
-  const fieldLabel = document.createElement('label');
-  fieldLabel.textContent = `Player ${PNumber} Name `;
-  newPlayerForm.appendChild(fieldLabel);
-
-  const playerName = document.createElement('INPUT');
-  playerName.setAttribute('id', `nameField-${PNumber}`);
-  playerName.setAttribute('type', 'text');
-  playerName.setAttribute('placeholder', 'Name');
-  newPlayerForm.appendChild(playerName);
-
-  const saveButtonDiv = document.getElementById('saveButtonDiv');
-
-  if (PNumber === 2) {
-    const submitButton = document.createElement('Button');
-    submitButton.addEventListener('click', playerCapture);
-    submitButton.textContent = 'Save';
-    submitButton.setAttribute('class', 'SaveButton');
-    submitButton.setAttribute('type', 'button');
-    submitButton.number = PNumber;
-
-    saveButtonDiv.appendChild(submitButton);
+  function writeInCell(i, j, value) {
+    document.getElementById(`${i}-${j}`).innerHTML = value;
   }
-};
 
-function enableBtn() {
-  document.getElementById('StartGameButton').disabled = false;
-}
+  function removePlayerForm(){
+    document.getElementById('buttonDiv').remove();
+    document.getElementById('saveButtonDiv').remove();
+    document.getElementById('myPlayerForm-1').innerHTML = '';
+    document.getElementById('myPlayerForm-2').innerHTML = '';
+    document.getElementById('StartGameButton').disabled = false;
+  }
 
-return {
-  formRender,
-  writeInCell,
-  enableBtn
-};
+  function displayPlayers() {
+    const playerDiv1 = document.getElementById('Player1');
+    const playerDiv2 = document.getElementById('Player2');
+
+    playerDiv1.innerHTML = `${player1.name}, You're ${player1.token}`;
+    playerDiv2.innerHTML = `${player2.name}, You're ${player2.token}`;
+  }
+
+  function getNameField(n) {
+    return document.getElementById(`nameField-${n}`).value;
+  }
+
+  function playerCapture() {
+    const name1 = getNameField(1);
+    const name2 = getNameField(2);
+    player1 = player(name1, 'X');
+    player2 = player(name2, 'O');
+
+    currentPlayer = player1;
+    removePlayerForm();
+    displayPlayers();
+  }
+
+  const formRender = (formDiv, PNumber) => {
+    formDiv.innerHTML = '';
+
+    const newPlayerForm = document.createElement('FORM');
+    newPlayerForm.setAttribute('id', `myPlayerForm-${PNumber}`);
+    formDiv.appendChild(newPlayerForm);
+
+    const fieldLabel = document.createElement('label');
+    fieldLabel.textContent = `Player ${PNumber} Name `;
+    newPlayerForm.appendChild(fieldLabel);
+
+    const playerName = document.createElement('INPUT');
+    playerName.setAttribute('id', `nameField-${PNumber}`);
+    playerName.setAttribute('type', 'text');
+    playerName.setAttribute('placeholder', 'Name');
+    newPlayerForm.appendChild(playerName);
+
+    const saveButtonDiv = document.getElementById('saveButtonDiv');
+
+    if (PNumber === 2) {
+      const submitButton = document.createElement('Button');
+      submitButton.addEventListener('click', playerCapture);
+      submitButton.textContent = 'Save';
+      submitButton.setAttribute('class', 'SaveButton');
+      submitButton.setAttribute('type', 'button');
+      submitButton.number = PNumber;
+
+      saveButtonDiv.appendChild(submitButton);
+    }
+  };
+
+  function enableBtn() {
+    document.getElementById('StartGameButton').disabled = false;
+  }
+
+  return {
+    formRender,
+    writeInCell,
+    enableBtn,
+  };
 
 }());
 
